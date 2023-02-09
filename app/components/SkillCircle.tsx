4b82603c4cb3545b5a79cdc10a4f58ec09a51ce9
Roberto Duran <shaggyrd+github@gmail.com>
2023-02-09 16:28:14 -0500

    add prisma conf

---
+++ app/components/SkillCircle.tsx
@@ -0,0 +1,26 @@
+
+type Props = {
+    title: string;
+    percentage: number;
+}
+export default function SkillCircle({ title, percentage }: Props) {
+    return (
+      <div className="circle-progress relative p-3 text-center md:p-5">
+          <div className="relative mb-2 inline-block text-primary">
+              <svg className="circle" width="120" height="120" viewBox="0 0 120 120">
+                  <circle className="circle-bg" cx="60" cy="60" r="54"
+                          pathLength="1" strokeDasharray="round"
+                          strokeWidth="12"></circle>
+                  <circle cx="60" cy="60" r="54" strokeWidth="12"
+                          opacity="1" pathLength="1" strokeDashoffset="0px"
+                          strokeDasharray={`0.${percentage}px 1px`}></circle>
+              </svg>
+              <span className="absolute left-1/2 top-1/2 -translate-y-1/2
+                    -translate-x-1/2 transform text-center text-xl text-body">
+                        {percentage}%
+                    </span>
+          </div>
+          <h5>{title}</h5>
+      </div>
+    );
+};
